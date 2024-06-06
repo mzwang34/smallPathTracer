6a94916a4a74a53ebb597af395affcde725dc2bd
mzwang34 <mzwang34@163.com>
2024-06-06 22:20:19 +0800

    update README

--- README.md
+++ README.md
@@ -56,4 +56,4 @@ make
 
 ## Acknowledgement
 
-The code is based on *[GAMES101](https://sites.cs.ucsb.edu/~lingqi/teaching/games101.html)*, *[Ray Tracing in One Weekend Book Series](https://github.com/RayTracing/raytracing.github.io)*, *[Howl's Blog](https://howl144.github.io/2023/09/30/00014.%20Games101%20FinalProject/#shadowing-masking-function)* and *[YANGTHEKING](https://blog.csdn.net/ycrsw/article/details/124408789)*.
+The code is based on *[GAMES101](https://sites.cs.ucsb.edu/~lingqi/teaching/games101.html)*, *[GAMES202](https://sites.cs.ucsb.edu/~lingqi/teaching/games202.html)*, *[Ray Tracing in One Weekend Book Series](https://github.com/RayTracing/raytracing.github.io)*, *[Howl's Blog](https://howl144.github.io/2023/09/30/00014.%20Games101%20FinalProject/#shadowing-masking-function)* and *[YANGTHEKING](https://blog.csdn.net/ycrsw/article/details/124408789)*.
---
+++ out/build/x64-Debug/CMakeFiles/RayTracing.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Documents/vision/course/smallPathTracer/BVH.cpp" "CMakeFiles/RayTracing.dir/BVH.cpp.obj" "gcc" "CMakeFiles/RayTracing.dir/BVH.cpp.obj.d"
+  "D:/Documents/vision/course/smallPathTracer/Renderer.cpp" "CMakeFiles/RayTracing.dir/Renderer.cpp.obj" "gcc" "CMakeFiles/RayTracing.dir/Renderer.cpp.obj.d"
+  "D:/Documents/vision/course/smallPathTracer/Scene.cpp" "CMakeFiles/RayTracing.dir/Scene.cpp.obj" "gcc" "CMakeFiles/RayTracing.dir/Scene.cpp.obj.d"
+  "D:/Documents/vision/course/smallPathTracer/Vector.cpp" "CMakeFiles/RayTracing.dir/Vector.cpp.obj" "gcc" "CMakeFiles/RayTracing.dir/Vector.cpp.obj.d"
+  "D:/Documents/vision/course/smallPathTracer/main.cpp" "CMakeFiles/RayTracing.dir/main.cpp.obj" "gcc" "CMakeFiles/RayTracing.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
